f744651dc507be31f60c3a4a7134d37d4304ab8f
Adolfo De Unánue <nanounanue@gmail.com>
2019-11-08 23:53:43 -0600

    Closes #8

--- vagrant-ds/bootstrap.sh
+++ vagrant-ds/bootstrap.sh
@@ -11,6 +11,9 @@ apt-get -y install postgresql-11 postgresql-11-postgis-2.5 postgis postgresql-11
 apt-get -y install curl zsh git sqlite3
 chsh -s /bin/zsh vagrant
 
+# R
+apt-get -y install r-base
+
 # pyenv
 apt-get -y install --no-install-recommends make build-essential libssl-dev zlib1g-dev\
                     libbz2-dev libreadline-dev libsqlite3-dev llvm libncurses5-dev\
