a21f432879dbccf6e450359509047a52ec0ada4a
Adolfo De Unánue <nanounanue@gmail.com>
2019-12-06 17:52:20 -0600

    Quité el cascarón a helpers.sql

--- berka/sql/helpers.sql
+++ berka/sql/helpers.sql
@@ -1,25 +1,3 @@
-\echo 'Berka(helpers)'
-\echo 'Programación para Ciencia de Datos'
-\echo 'Adolfo De Unánue <unanue@itam.mx>'
-\set VERBOSITY terse
-\set ON_ERROR_STOP true
-
-do language plpgsql $$ declare
-    exc_message text;
-    exc_context text;
-    exc_detail text;
-begin
-  raise notice 'adding extensions';
-  raise notice 'creating functions';
-
-exception when others then
-    get stacked diagnostics exc_message = message_text;
-    get stacked diagnostics exc_context = pg_exception_context;
-    get stacked diagnostics exc_detail = pg_exception_detail;
-    raise exception E'\n------\n%\n%\n------\n\nCONTEXT:\n%\n', exc_message, exc_detail, exc_context;
-end $$;
-
-
 create or replace function fix_date
   (
     bad_date text
